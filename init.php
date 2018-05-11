cf57d0ca8b8900b58e2af2ee05c13eb285d320bb
imbafym <707082902@qq.com>
2018-05-11 12:07:59 +1000

    Added DB Config 3

--- init.php
+++ init.php
@@ -1,13 +1,10 @@
 <?php
 
 
+require "database.php";
 
 
-$db_name="project";
-$mysql_user="root";
-$server_name="localhost";
-
-$connection=mysqli_connect($server_name,$mysql_user,"",$db_name);
+$connection=mysqli_connect($db['default']);
 
 if(!$connection){
   echo "Connection not successful";
