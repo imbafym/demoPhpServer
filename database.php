3fe89274f02dd587157c3e3636ddc264d1f787bb
imbafym <707082902@qq.com>
2018-05-11 12:50:01 +1000

    Added DB Config 3

--- database.php
+++ database.php
@@ -9,26 +9,40 @@ $cleardb_db       = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-    'dsn'   => '',
-    'hostname' => $cleardb_server,
-    'username' => $cleardb_username,
-    'password' => $cleardb_password,
-    'database' => $cleardb_db,
-    'dbdriver' => 'mysqli',
-    'dbprefix' => '',
-    'pconnect' => FALSE,
-    'db_debug' => (ENVIRONMENT !== 'production'),
-    'cache_on' => FALSE,
-    'cachedir' => '',
-    'char_set' => 'utf8',
-    'dbcollat' => 'utf8_general_ci',
-    'swap_pre' => '',
-    'encrypt' => FALSE,
-    'compress' => FALSE,
-    'stricton' => FALSE,
-    'failover' => array(),
-    'save_queries' => TRUE
-);
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$connection = new mysqli($server, $username, $password, $db);
+
+if(!$connection){
+  echo "Connection not successful v2";
+}else{
+  echo "Connection successful";
+}
+//
+//$db['default'] = array(
+//    'dsn'   => '',
+//    'hostname' => $cleardb_server,
+//    'username' => $cleardb_username,
+//    'password' => $cleardb_password,
+//    'database' => $cleardb_db,
+//    'dbdriver' => 'mysqli',
+//    'dbprefix' => '',
+//    'pconnect' => FALSE,
+//    'db_debug' => (ENVIRONMENT !== 'production'),
+//    'cache_on' => FALSE,
+//    'cachedir' => '',
+//    'char_set' => 'utf8',
+//    'dbcollat' => 'utf8_general_ci',
+//    'swap_pre' => '',
+//    'encrypt' => FALSE,
+//    'compress' => FALSE,
+//    'stricton' => FALSE,
+//    'failover' => array(),
+//    'save_queries' => TRUE
+//);
 
 ?>
