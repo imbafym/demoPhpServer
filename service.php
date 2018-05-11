60014527ec14a84efe0d334c5ac51b94ac425e16
imbafym <707082902@qq.com>
2018-05-11 23:40:27 +1000

    Added DB Config 3

---
+++ service.php
@@ -0,0 +1,22 @@
+<?php
+
+require "database.php";
+//connect to the data base
+
+
+//fetch data from table food in the database
+$query = "SELECT * FROM SERVICE";
+$result = mysqli_query($connection, $query);
+
+//create an array
+$emparray = array();
+
+if(mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+       $emparray[] = $row;
+    }
+}
+
+echo json_encode($emparray);
+
+?>
